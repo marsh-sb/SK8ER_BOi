c7330e308a6ec91b7616f93edd969be7661f0ec1
marsh-sb <enotaku5526@gmail.com>
2021-01-31 06:47:39 +0000

    レイアウト一部修正(トップ、ユーザー、投稿)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def update
     @user = User.find(current_user.id)
     @user.update(user_params)
     redirect_to user_path(current_user.id)
+  end
+
+  def edit
+    @user = current_user
+    if @user == current_user
+     render "edit"
+    else
+     redirect_to posts_path
+    end
   end
 
   private
