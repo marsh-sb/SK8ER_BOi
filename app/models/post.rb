44fc2666540c9b48d2cf16a3f4a30e18b81491ee
marsh-sb <enotaku5526@gmail.com>
2021-02-02 08:28:37 +0000

    [Add]検索機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,12 @@ class Post < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  def Post.search(search, user_or_post)
+    if user_or_post == "2"
+       Post.where(['title LIKE ?', "%#{search}%"])
+    else
+       Post.all
+    end
+  end
+
 end
