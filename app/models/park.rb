e572436cc53684c2c63daabf45946445230028fa
marsh-sb <enotaku5526@gmail.com>
2021-02-13 08:27:11 +0000

    park_validates_fix

--- app/models/park.rb
+++ app/models/park.rb
@@ -18,7 +18,9 @@ class Park < ApplicationRecord
     uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=#{ENV['API_KEY']}")
     res = HTTP.get(uri).to_s
     response = JSON.parse(res)
-    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    if response["results"].present?
+      self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+      self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    end
   end
 end
